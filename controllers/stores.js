863794de6c1ff4f4507078065f595936ec5a4059
MengWei <webgod2006@gmail.com>
2012-05-22 17:41:58 +0800

    增加商户门店相关页面。

---
+++ controllers/stores.js
@@ -0,0 +1,13 @@
+/*var tag_ctrl = require('./tag');
+var user_ctrl = require('./user');
+var topic_ctrl = require('./topic');
+var config = require('../config').config;
+var EventProxy = require('eventproxy').EventProxy;
+
+var sanitize = require('validator').sanitize;
+*/
+
+exports.index = function(req,res,next){
+    res.render('stores/stores');
+//    res.redirect('/signin');
+};

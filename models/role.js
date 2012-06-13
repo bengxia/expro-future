36754d8214ba0c29261d13ee8e78d381e8d8ed22
MengWei <webgod2006@gmail.com>
2012-06-13 09:09:26 +0800

    实现员工管理列表页面。

--- models/role.js
+++ models/role.js
@@ -1,20 +1,1 @@
-var mysql = require('../libs/mysql.js');
-
-function create() {
-    return new Role();
-};
-
-function Role() {
-};
-
-Role.prototype.findOne = function(opt, cb) {
-    var sql = 'SELECT t1.*, t3.* FROM ef_role as t1, ef_role_privilage as t2, ef_route_table as t3 '+
-              'WHERE t1._id = t2.role_id AND t3._id = t2.route_table_id AND t1._id = '+opt.role;
-    mysql.query(sql, function(err, rs) {
-        if(err) return cb(err);
-        if(!rs.length) return cb(err);
-        cb(err, rs[0]);
-    });
-};
-
-exports = module.exports = create;
+var mysql = require('../libs/mysql.js');function create() {    return new Role();};function Role() {    this.table = 'ef_role';};Role.prototype.findOne = function(opt, cb) {    var sql = 'SELECT t1.*, t3.* FROM ef_role as t1, ef_role_privilage as t2, ef_route_table as t3 '+              'WHERE t1._id = t2.role_id AND t3._id = t2.route_table_id AND t1._id = '+opt.role;    mysql.query(sql, function(err, rs) {        if(err) return cb(err);        if(!rs.length) return cb(err);        cb(err, rs[0]);    });};Role.prototype.findAll = function(opt, cb) {    var sql = " SELECT * FROM ef_role  "        +" where 1=1 ";    if(opt.where){        sql += opt.where;    }    if(opt.sidx && opt.sord){        sql += " ORDER BY "+opt.sidx+" "+opt.sord;    }    if(opt.start && opt.limit){        sql += " LIMIT "+ opt.start + " , "+opt.limit;    }    mysql.query(sql, function(err, rs) {        if(err) return cb(err);        if(!rs.length) return cb(err);        cb(err, rs);    });};exports = module.exports = create;
